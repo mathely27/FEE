1c221c3520fd0eb049c010bd8a9e4a4b16e1ff40
mathely27 <mathely27@gmail.com>
2026-07-28 23:24:51 +0530

    add todo list

---
+++ todo/todo.js
@@ -0,0 +1,75 @@
+console.log("hello");
+
+const task=document.getElementById("taskInput");
+const add=document.getElementById("addBtn");
+const list = document.getElementById("taskList");
+const pending=document.getElementById("pendingCount");
+
+let count=0;
+
+add.addEventListener("click",function(){
+    if(task.value.trim()==""){
+        alert("Please Enter a valid task!!");
+        return;
+    }
+        const taskCard=document.createElement("div");
+
+        const taskText=document.createElement("p");
+        
+
+        taskText.innerText=task.value;
+
+        const completeBtn=document.createElement("button");
+        completeBtn.innerText = "✓";
+
+        completeBtn.addEventListener("click",function(){
+            if(taskText.style.textDecoration=="line-through"){
+                taskText.style.textDecoration="";
+                count++;
+                pending.innerText="pending : "+count;
+            }else{
+                taskText.style.textDecoration="line-through";
+                count--;
+                pending.innerText="pending : "+count;
+            }
+            
+        })
+
+        const editBtn = document.createElement("button");
+        editBtn.innerText = "Edit";
+
+        const deleteBtn = document.createElement("button");
+        deleteBtn.innerText = "Delete";
+            // list.append(taskCard);
+
+        deleteBtn.addEventListener("click",function(){
+             if (taskText.style.textDecoration == "line-through") {
+
+             } else {
+                count--;
+                pending.innerText = "Pending : " + count;
+
+            }
+            deleteBtn.parentElement.remove();
+        })    
+
+
+
+
+        taskCard.append(taskText);
+
+// editBtn.innerText = "Edit";
+        taskCard.append(editBtn);
+
+// deleteBtn.innerText = "Delete";
+        taskCard.append(deleteBtn);
+
+// completeBtn.innerText = "✓";
+        taskCard.append(completeBtn);
+
+        count++;
+        pending.innerText="pending : "+count;
+        list.append(taskCard);
+
+});
+
